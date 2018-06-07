f027efd98ae60505ca6cf475a0d0553f35ef750f
Peter Estes <peternigelestes@gmail.com>
2018-06-07 16:39:41 -0400

    everything running

--- todo.js
+++ todo.js
@@ -1,5 +1,18 @@
-const R = require("ramda");
+const { head } = require("ramda")
 
-const things = ["foo", 8, null, false];
+const [execPath, jsPath, cmd, ...rest] = process.argv
 
-console.log(R.head(things));
+console.log(handleCmd(cmd))
+
+function handleCmd(cmd) {
+	switch (cmd) {
+		case "init":
+			return "you asked for init cmd"
+		case "add":
+			return "you asked for add cmd"
+		case "rm":
+			return "you asked for rm cmd"
+		default:
+			return `${cmd} is not supported`
+	}
+}
